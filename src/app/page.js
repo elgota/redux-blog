5ab35919e985cacd1ff5524ab9c72caf3d5ac0e6
elgota <elgota8@gmail.com>
2024-07-05 10:46:29 -0300

    Learning data structure

--- src/app/page.js
+++ src/app/page.js
@@ -4,11 +4,14 @@ import "./globals.css";
 import { store } from "./store";
 import { Provider } from "react-redux";
 import Counter from "./features/counter/Counter";
-
+import  PostsList from "./features/posts/PostsList";
+import AddPostForm from "./features/posts/AddPostForm";
 export default function Home() {
   return (
     <Provider store={store}>
-      <Counter/>
+      {/* <Counter/> */}
+      <PostsList/>
+      <AddPostForm/>
     </Provider>
   );
 }
